9e89b8a26871e864ccce792df6eb4f3989b94fe8
CarlosZ96 <c4r1oszambrano@gmail.com>
2024-07-10 10:30:18 -0500

    Feature:
    creating the skeleton of the https request

---
+++ src/redux/euro/euroSlice.jsx
@@ -0,0 +1,20 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import React from 'react';
+
+export const euroSlice = createAsyncThunk(
+  'users/fetchUsers',
+  async (_, { rejectWithValue }) => {
+    try {
+      const response = await fetch('https://jsonplaceholder.typicode.com/users');
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
+
+export default euroSlice;
