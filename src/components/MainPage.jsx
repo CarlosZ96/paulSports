6fb47f7fb8bc42e78adffd9be94748d99c77ee3c
CarlosZ96 <c4r1oszambrano@gmail.com>
2024-07-28 14:44:10 -0500

    Firebase config
    Firebase config

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,10 +1,37 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import apifirebase from 'firebase';
-import {getAuth, onAuthStateChanged} from 'firebase/auth'
-const auth = getAuth(apifirebase)
+import { initializeApp } from 'firebase/app';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
 function MainPage() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // El usuario ha iniciado sesión
+        dispatch({ type: 'USER_LOGGED_IN', payload: user });
+      } else {
+        // El usuario ha cerrado sesión
+        dispatch({ type: 'USER_LOGGED_OUT' });
+      }
+    });
+
+    return () => unsubscribe();
+  }, [dispatch]);
+
 
   return (
     <div>
@@ -13,7 +40,7 @@ function MainPage() {
         <div></div>
       </header>
       <section>
- 
+
       </section>
     </div>
   );
